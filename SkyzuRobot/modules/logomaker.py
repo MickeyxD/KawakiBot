a7dc6be221244da59442562433df40bd561ae829
♡мιcкεү♡ <88245294+MickeyxD@users.noreply.github.com>
2022-01-30 19:38:15 +0530

    Update logomaker.py

--- SkyzuRobot/modules/logomaker.py
+++ SkyzuRobot/modules/logomaker.py
@@ -114,7 +114,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Skyzu Robot](https://t.me/SkyzuRobot)",
+            caption="Logo by [𝐕ᴀᴍᴘɪʀᴇャ](https://t.me/VampireXRobot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -198,7 +198,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Skyzu Robot](https://t.me/SkyzuRobot)",
+            caption="Logo by [𝐕ᴀᴍᴘɪʀᴇャ](https://t.me/VampireXRobot)",
             force_document=False,
         )
         os.remove(flnme)
